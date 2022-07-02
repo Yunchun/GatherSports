9104ae2b5b444c88731cdcc2969b4199c7907ce0
Yunchun <yup086@ucsd.edu>
2022-07-02 11:07:26 -0500

    Delete src directory

--- src/components/login/Container.js
+++
@@ -1,7 +0,0 @@
-export default function Container({ children }) {
-    return (
-        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-            <div>{children}</div>
-        </div>
-    );
-}
